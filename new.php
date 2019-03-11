2de715191b0e4917b9d0cc3f949a823ad30c86fd
Mike Annibale <mannibale81@gmail.com>
2019-03-10 20:33:08 -0400

    Add files via upload

---
+++ new.php
@@ -0,0 +1,15 @@
+<?php # Script 3.5 - New.php
+
+  $page_title = 'New php';
+
+  include ('includes/header.html');
+
+?>
+
+<?php
+
+  // Include the footer:
+
+  include ('includes/footer.html');
+
+?>
